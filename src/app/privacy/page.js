32bf9145b7fc3cd643fea47fca061dc00a936d63
Evren <evren@192.168.1.119>
2023-07-10 22:08:54 +0300

    first commit

---
+++ src/app/privacy/page.js
@@ -0,0 +1,62 @@
+const Privacy = () => {
+  return (
+    <div className="container mx-auto py-8">
+      <h1 className="text-2xl font-bold mb-4">Privacy Policy</h1>
+      <p className="mb-4">PRIVACY POLICY for "Guess On! Forehead Words"</p>
+      <p>
+        This Privacy Policy governs the manner in which "Guess On! Forehead
+        Words" collects, uses, maintains and discloses information collected
+        from users (each, a "User") of the mobile application. This privacy
+        policy applies to the App and all products and services offered by
+        "Guess On! Forehead Words".
+      </p>
+
+      <h2 className="text-xl font-bold mt-8 mb-4">
+        Personal identification information
+      </h2>
+      <p>
+        We may collect personal identification information from Users in a
+        variety of ways, including, but not limited to, when Users visit our
+        app, register on the app, and in connection with other activities,
+        services, features or resources we make available on our App.
+      </p>
+
+      <h2 className="text-xl font-bold mt-8 mb-4">
+        Non-personal identification information
+      </h2>
+      <p>
+        We may collect non-personal identification information about Users
+        whenever they interact with our App. Non-personal identification
+        information may include the device's name, operating system, and other
+        similar information.
+      </p>
+
+      <h2 className="text-xl font-bold mt-8 mb-4">
+        How we use collected information
+      </h2>
+      <p>
+        "Guess On! Forehead Words" may collect and use Users' personal
+        information for the following purposes: To improve customer service, To
+        personalize user experience, To improve our App, To send periodic
+        emails.
+      </p>
+
+      <h2 className="text-xl font-bold mt-8 mb-4">
+        How we protect your information
+      </h2>
+      <p>
+        We adopt appropriate data collection, storage and processing practices
+        and security measures to protect against unauthorized access,
+        alteration, disclosure or destruction of your personal information,
+        username, password, transaction information and data stored on our App.
+      </p>
+
+      <p className="mt-4">
+        For more details, please refer to the full privacy policy on our
+        website.
+      </p>
+    </div>
+  );
+};
+
+export default Privacy;
---
+++ src/app/user-agreement/page.js
@@ -0,0 +1,65 @@
+const UserAgreement = () => {
+  return (
+    <div className="container mx-auto py-8">
+      <h1 className="text-2xl font-bold mb-4">
+        Application Membership and User Agreement
+      </h1>
+      <p className="mb-4">
+        "Guess On! Forehead Words" Application Membership and User Agreement
+      </p>
+
+      <h2 className="text-xl font-bold mt-8 mb-4">
+        Acceptance of the Agreement
+      </h2>
+      <p>
+        By downloading, installing, or using the Application, the User accepts
+        the terms of this agreement.
+      </p>
+
+      <h2 className="text-xl font-bold mt-8 mb-4">Use of the Application</h2>
+      <p>
+        The User agrees to use the Application in accordance with all applicable
+        laws and regulations. The User will not use the Application for any
+        unlawful or prohibited activities.
+      </p>
+
+      <h2 className="text-xl font-bold mt-8 mb-4">User Responsibilities</h2>
+      <p>
+        The User is responsible for all actions taken within the Application.
+        The User agrees to keep their login information confidential and not to
+        share it with any third parties.
+      </p>
+
+      <h2 className="text-xl font-bold mt-8 mb-4">
+        Intellectual Property Rights
+      </h2>
+      <p>
+        The Application and all related content, including but not limited to,
+        text, graphics, logos, and software, are the property of the Company and
+        are protected by international copyright laws.
+      </p>
+
+      <h2 className="text-xl font-bold mt-8 mb-4">Termination</h2>
+      <p>
+        The Company reserves the right to terminate the User's access to the
+        Application at any time, for any reason, without notice.
+      </p>
+
+      <h2 className="text-xl font-bold mt-8 mb-4">Changes to the Agreement</h2>
+      <p>
+        The Company reserves the right to modify the terms of this agreement at
+        any time. Any changes will be posted within the Application, and the
+        User's continued use of the Application following any changes
+        constitutes acceptance of those changes.
+      </p>
+
+      <h2 className="text-xl font-bold mt-8 mb-4">Governing Law</h2>
+      <p>
+        This agreement shall be governed by and construed in accordance with the
+        laws of the jurisdiction in which the Company is located.
+      </p>
+    </div>
+  );
+};
+
+export default UserAgreement;
